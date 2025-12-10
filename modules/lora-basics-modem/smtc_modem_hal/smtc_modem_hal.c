b3ebc5a81b791770d3f4aaf157482ad639f3b10d
Carlo Caione <carlo.caione@gmail.com>
2025-12-10 22:15:56 +0100

    wip
    Signed-off-by: Carlo Caione <carlo.caione@gmail.com>

--- modules/lora-basics-modem/smtc_modem_hal/smtc_modem_hal.c
+++ modules/lora-basics-modem/smtc_modem_hal/smtc_modem_hal.c
@@ -6,9 +6,19 @@
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/lora.h>
+#include <zephyr/drivers/gpio.h>
 
+#include <lbm_common.h>
 #include <smtc_modem_hal.h>
 #include <smtc_modem_hal_ext.h>
+
+typedef void (*dio_callback_t)(void *context);
+
+struct cb_data_t {
+	struct gpio_callback cb;
+	dio_callback_t dio_cb;
+	void *context;
+};
 
 static const struct device *prv_transceiver_dev;
 
@@ -20,7 +30,48 @@ void smtc_modem_hal_init(const struct device *transceiver)
 	prv_transceiver_dev = transceiver;
 }
 
-void smtc_modem_hal_irq_config_radio_irq(void (*callback)(void *context), void *context)
+static void hal_irq_callback(const struct device *port, struct gpio_callback *cb, uint32_t pins)
 {
+	struct cb_data_t *data = CONTAINER_OF(cb, struct cb_data_t, cb);
+
+	if (data->dio_cb != NULL) {
+		data->dio_cb(data->context);
+	}
+}
+
+void smtc_modem_hal_irq_config_radio_irq(dio_callback_t dio_cb, void *context)
+{
+	static struct cb_data_t cb_data;
+
+	cb_data.dio_cb = dio_cb;
+	cb_data.context = context;
+
+	gpio_init_callback(&cb_data.cb, hal_irq_callback, 0);
+	lbm_driver_add_dio1_gpio_callback(prv_transceiver_dev, &cb_data.cb);
 
 }
+
+void smtc_modem_hal_start_radio_tcxo(void)
+{
+	/*
+	 * We only support TCXO's that are wired to the transceiver. In such cases,
+	 * this function must be empty. See 5.25 of the porting guide.
+	 */
+}
+
+void smtc_modem_hal_set_ant_switch(bool is_tx_on)
+{
+	/*
+	 * From the porting guide:
+	 * If no antenna switch is used then implement an empty command.
+	 */
+}
+
+void smtc_modem_hal_stop_radio_tcxo(void)
+{
+	/*
+	 * We only support TCXO's that are wired to the transceiver. In such cases,
+	 * this function must be empty. See 5.26 of the porting guide.
+	 */
+}
+
